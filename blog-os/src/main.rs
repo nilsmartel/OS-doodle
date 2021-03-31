9594729243cd130e40b2f3f836f5d3353314980f
Nils Martel <nils.martel@graphmasters.net>
2021-03-31 21:31:37 +0200

    Do fun stuff with the vga buffer

--- blog-os/src/main.rs
+++ blog-os/src/main.rs
@@ -1,7 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-
 mod vga_buffer;
 use core::panic::PanicInfo;
 
@@ -13,6 +12,33 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    let mut writer = vga_buffer::WRITER.lock();
+
+    writer.write_string("Hello!");
+    wait(300000000);
+    writer.write_string("\nYou're awake.");
+    wait(300000000);
+
+    for b in b"\nYou've been sleeping for a long time" {
+        writer.write_byte(*b);
+        wait(10000000);
+    }
+    for b in b"...\n\n" {
+        wait(100000000);
+        writer.write_byte(*b);
+    }
+
+    for b in b"\nIt's no longer safe here.\n\nWe must go now." {
+        writer.write_byte(*b);
+        wait(20000000);
+    }
+
     loop {}
+}
+
+fn wait(mut cycles: usize) {
+    while cycles != 0 {
+        cycles -= 1;
+    }
 }
