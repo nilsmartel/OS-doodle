c25f44f482db592d9b06c304712eb7559b44ae6a
Nils Martel <nils.martel@graphmasters.net>
2021-03-31 20:54:18 +0200

    Start working on VGA Buffer

---
+++ blog-os/README.md
@@ -0,0 +1,4 @@
+# Rust OS (Blog OS)
+
+All code is a direct copy of https://os.phil-opp.com.
+It's a beautiful tutorial.
--- blog-os/src/main.rs
+++ blog-os/src/main.rs
@@ -1,5 +1,7 @@
 // src/main.rs
 
+mod vga_buffer;
+
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
